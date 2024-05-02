ff13e363628a7e4d2b8c30c1269dedb8bc311040
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-05-02 13:07:00 +0530

    refactor(users): finished brave onfig

---
+++ snigdhaos-brave-config/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+sourcefiles="Brave-Browser"
+
+pkgname=$(grep "^pkgname=" PKGBUILD | awk -F"=" '{print $2}')
+pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
+arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
+
+tar -zcvf $pkgname.tar.gz $sourcefiles 
